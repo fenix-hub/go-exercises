aa1927a1c6ee1b92b08e73363340fcd3905317c1
Nicolò Santilio <nicolo.santilio@outlook.com>
2026-04-17 17:53:20 +0200

    Create rot13reader.go

---
+++ tour-of-go/rot13reader.go
@@ -0,0 +1,31 @@
+// Ref: https://go.dev/tour/methods/23
+
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (reader *rot13Reader) Read(b []byte) (int, error) {
+	n, err := reader.r.Read(b)
+	for i := range n {
+		if (b[i] >= 65 && b[i] <= 77 || b[i] >= 97 && b[i] <= 109) {
+			b[i] += 13
+		} else if (b[i] >= 78 && b[i] <= 90 || b[i] >= 110 && b[i] <= 122) {
+			b[i] -= 13
+		}
+	}
+	return n, err
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
